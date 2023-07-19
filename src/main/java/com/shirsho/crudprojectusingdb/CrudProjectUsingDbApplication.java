c24a8608ee107ec55a9e9fe25030915b49cf445d
Shirshaditya Gupta <shirshaditya.gupta@gmail.com>
2023-07-19 13:52:30 -0600

    Initialize repo - commit

---
+++ src/main/java/com/shirsho/crudprojectusingdb/CrudProjectUsingDbApplication.java
@@ -0,0 +1,13 @@
+package com.shirsho.crudprojectusingdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudProjectUsingDbApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CrudProjectUsingDbApplication.class, args);
+  }
+
+}
