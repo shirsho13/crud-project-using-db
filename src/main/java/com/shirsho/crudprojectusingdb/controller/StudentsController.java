c6d0031fa3e6afe4bc8d76bdd60256ac94380d09
Shirshaditya Gupta <shirshaditya.gupta@gmail.com>
2023-07-19 15:19:56 -0600

    Updated controller
    - Added the StudentDto data model
    - Added POST method
    - Restructured packages

--- src/main/java/com/shirsho/crudprojectusingdb/controller/StudentsController.java
+++ src/main/java/com/shirsho/crudprojectusingdb/controller/StudentsController.java
@@ -1,11 +1,24 @@
 package com.shirsho.crudprojectusingdb.controller;
 
+import com.shirsho.crudprojectusingdb.model.StudentDto;
+import com.shirsho.crudprojectusingdb.model.dao.Student;
 import com.shirsho.crudprojectusingdb.service.StudentRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class StudentsController {
     private final StudentRepository studentRepository;
+
+    @PostMapping("/students")
+    public void postStudent(@RequestBody StudentDto studentDto) {
+        Student student=new Student();
+        student.setFirstName(studentDto.getFirstName());
+        student.setLastName(studentDto.getLastName());
+        student.setYear(studentDto.getYear());
+        studentRepository.save(student);
+    }
 }
---
+++ src/main/java/com/shirsho/crudprojectusingdb/model/StudentDto.java
@@ -0,0 +1,10 @@
+package com.shirsho.crudprojectusingdb.model;
+
+import lombok.Data;
+
+@Data
+public class StudentDto {
+    private String firstName;
+    private String lastName;
+    private int year;
+}
